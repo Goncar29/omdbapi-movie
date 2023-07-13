a76e923b932f6113addd44a3a9d29d4e0fb5c675
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-12 21:10:17 -0300

    Creamos archivos con la info de la api dada, renderizamos en App esa info

--- omdbapi-movie/src/App.jsx
+++ omdbapi-movie/src/App.jsx
@@ -1,10 +1,34 @@
+import './App.css';
+import responseMovies from './mocks/results.json';
+import noResults from './mocks/no-Results.json';
 
 function App() {
-
+    const movies = responseMovies.Search
+    const hasMovies = movies?.lenght > 0
     return (
-        <h1>
-            Prueba
-        </h1>
+        <div className="page">
+            <header>
+                <h1>Buscador de peliculas</h1>
+                <form className="form">
+                    <input placeholder="Avengers, Star Wars, Matrix ..." />
+                    <button type="submit">Buscar</button>
+                </form>
+            </header>
+
+            <main>
+                <ul>
+                    {
+                        movies.map(movie =>(
+                            <li key={movie.imdbID}>
+                                <h3>{movie.Title}</h3>
+                                <p>{movie.Year}</p>
+                                <img src={movie.Poster} alt={movie.Title}></img>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </main>
+        </div>
     )
 }
 
