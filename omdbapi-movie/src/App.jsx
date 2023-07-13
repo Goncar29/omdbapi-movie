54969e16414e5081e229c1e2a6b44d7c8aaed223
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-13 14:26:00 -0300

    usamos useRef para el formulario

--- omdbapi-movie/src/App.jsx
+++ omdbapi-movie/src/App.jsx
@@ -1,23 +1,31 @@
 import './App.css';
 import { useMovies } from './hooks/useMovies';
 import { Movies } from './components/Movies';
+import { useRef } from 'react';
 // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=5acb29ce
 
 function App() {
-    const { movies: mappedMovies } = useMovies();
+    const { movies } = useMovies();
+    const inputRef = useRef()
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const inputEl = inputRef.current
+        const value = inputEl.value
+        alert(value)
+    }
     return (
         <div className="page">
             <header>
                 <h1>Buscador de peliculas</h1>
-                <form className="form">
-                    <input placeholder="Avengers, Star Wars, Matrix ..." />
+                <form className="form" onSubmit={handleSubmit}>
+                    <input ref={inputRef} placeholder="Avengers, Star Wars, Matrix ..." />
                     <button type="submit">Buscar</button>
                 </form>
             </header>
 
             <main>
-                <Movies movies={mappedMovies} />
+                <Movies movies={movies} />
             </main>
         </div>
     )
