7df6ba5485fa18da6889b8ec18263d316d3b9954
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-07-13 15:52:12 -0300

    cambiamos el useRef para cambiarlo de la forma no controlada por react

--- omdbapi-movie/src/App.jsx
+++ omdbapi-movie/src/App.jsx
@@ -1,27 +1,55 @@
 import './App.css';
 import { useMovies } from './hooks/useMovies';
 import { Movies } from './components/Movies';
-import { useRef } from 'react';
+import { useEffect, useState } from 'react';
 // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=5acb29ce
 
 function App() {
     const { movies } = useMovies();
-    const inputRef = useRef()
+    const [query, setQuery] = useState('');
+    const [error, setError] = useState(null);
+
+    console.log('Render')
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const inputEl = inputRef.current
-        const value = inputEl.value
-        alert(value)
+        console.log({ query })
     }
+
+    const handleChange = (event) => {
+        const newQuery = event.target.value
+        if (newQuery.startsWith(' ')) return
+        setQuery(event.target.value)
+    }
+
+    useEffect(() => {
+        if (query === '') {
+            setError('Nada que buscar')
+            return
+        }
+        if (query.length < 3) {
+            setError('Escribe mas de 3 caracteres')
+            return
+        }
+        setError(null)
+    })
+
     return (
         <div className="page">
             <header>
                 <h1>Buscador de peliculas</h1>
                 <form className="form" onSubmit={handleSubmit}>
-                    <input ref={inputRef} placeholder="Avengers, Star Wars, Matrix ..." />
+                    <input
+                        style={{
+                            border: '1px solid transparent',
+                            borderColor: error ? 'red' : 'transparent'
+                        }} 
+                        onChange={handleChange} value={query} name='query' 
+                        placeholder="Avengers, Star Wars, Matrix ..." 
+                    />
                     <button type="submit">Buscar</button>
                 </form>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
             </header>
 
             <main>
