f7dbae50e039d5b79fd2e4190866f2a69a0405d1
Carlos Gonzalez <51546329+Goncar29@users.noreply.github.com>
2024-11-26 16:36:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://goncar29.github.io/omdbapi-movie/'
+  base: 'https://goncar29.github.io/omdbapi-movie',
 })
